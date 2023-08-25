223f4ab53a48d22e5b68a1f3aa727c64db48c06b
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-08-25 18:52:41 +0530

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,32 @@
-import React from "react";
+// Navbar.js
+import React, { useState } from "react";
 import "./Navbar.css";
 import { scrollToSection } from "./utils/scroller";
-import { FiGithub } from "react-icons/fi";
+import { FiGithub } from "react-icons/fi"; // Import FaBars for mobile menu icon
+import { FaBars } from "react-icons/fa";
 import sg from "../assets/sg2.jpg";
+
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className="nav">
+    <nav className={`nav ${isMobileMenuOpen ? "mobile-menu-open" : ""}`}>
       <img src={sg} alt="" />
-      <div className="nav-part2">
+      {/* Desktop menu */}
+      <div className="nav-part2 desktop-menu">
         <h4>Home</h4>
         <h4 onClick={() => scrollToSection("about")}>About</h4>
         <h4 onClick={() => scrollToSection("projects")}>Projects</h4>
         <h4 onClick={() => scrollToSection("second")}>Skills</h4>
         <h4 onClick={() => scrollToSection("contact")}>Contact</h4>
+      </div>
+      {/* Mobile menu */}
+      <div className="mobile-menu-icon" onClick={toggleMobileMenu}>
+        <FaBars size={30} />
       </div>
       <div className="nav-part3">
         <a
